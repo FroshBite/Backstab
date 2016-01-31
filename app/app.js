604f9a64a1149c812940264941300dd4c1fded3d
Kevin Xing <kxing@uwaterloo.ca>
2016-01-31 12:28:12 -0500

    map added

--- app/app.js
+++ app/app.js
@@ -4,6 +4,9 @@ var cursors;
 
 function preload() {
   game.load.spritesheet('player', 'assets/sprites/characters/male_warrior.png',60, 60,-1,3,4);
+  game.load.tilemap('test', 'assets/maps/test.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.image('tiles1', 'assets/maps/protected_assets/sprites/map/dungeon.png');
+  game.load.image('tiles2', 'assets/maps/protected_assets/sprites/blocks/dungeon_blocks2.png');
 }
 
 function create() {
@@ -11,7 +14,19 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     // phaser-illuminated interface library
     game.plugins.add(Phaser.Plugin.PhaserIlluminated);
-    
+
+    //map stuff
+    game.map = game.add.tilemap('test');
+    console.log("attempting first sheet");
+    game.map.addTilesetImage('tiles', 'tiles1');
+    console.log("attempting second sheet");
+    game.map.addTilesetImage('dungeon', 'tiles2');
+    game.backgroundlayer = game.map.createLayer('backgroundLayer');
+    game.blockedlayer = game.map.createLayer('blockedLayer');
+    game.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
+    game.backgroundlayer.resizeWorld();
+
+    // player stuff
     player=new Player(game, 'player');
 
     cursors = game.input.keyboard.createCursorKeys();
