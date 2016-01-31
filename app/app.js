48d40d0de0196f07429001c97397ef48d8ef3928
Serge Babayan <serj.b.babayan@gmail.com>
2016-01-30 22:53:31 -0500

    Added a second player for testing

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var player1;
+var player2;
 var cursors;
+var w_key,a_key, s_key, d_key;
 
 function preload() {
   game.load.spritesheet('player', 'assets/sprites/characters/male_warrior.png',60, 60,-1,3,4);
@@ -10,13 +12,20 @@ function create() {
   //  We're going to be using physics, so enable the Arcade Physics system
   game.physics.startSystem(Phaser.Physics.ARCADE);
   
-  player=new Player(game, 'player');
+  player1=new Player(game, 'player');
+  player2=new Player(game,'player');
 
   cursors = game.input.keyboard.createCursorKeys();
   var c_key=game.input.keyboard.addKey(Phaser.Keyboard.C); //the c key
-  c_key.onDown.add(player.attackKnife,player,1);
+  c_key.onDown.add(player1.attackKnife,player1,1);
+
+  w_key=game.input.keyboard.addKey(Phaser.Keyboard.W); //the c key
+  a_key=game.input.keyboard.addKey(Phaser.Keyboard.A); //the c key
+  s_key=game.input.keyboard.addKey(Phaser.Keyboard.S); //the c key
+  d_key=game.input.keyboard.addKey(Phaser.Keyboard.D); //the c key
 }
 
 function update() {
-  player.movePlayer(cursors.left,cursors.up,cursors.down,cursors.right);
+  player1.movePlayer(cursors.left,cursors.up,cursors.down,cursors.right);
+  player2.movePlayer(a_key,w_key,s_key,d_key);
 }
